313957423d76512eead1e8c34cc1b52d17b2c70f
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-11-14 14:38:28 +0100

    implemented

---
+++ src/components/Advertisements.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../components/styles/Ads.module.css'
+import AdCards from './AdCards'
+const Ads = () => {
+  return (
+    <div className={styles.container} >
+      <AdCards />
+    </div>
+  )
+}
+
+export default Ads
