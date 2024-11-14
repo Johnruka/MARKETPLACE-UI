5d7002a98fe92acde641cddeb977efc1306bb221
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-11-14 14:33:02 +0100

    modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Ads from './components/Advertisements'
 
-const App = () => {
+function App() {
+
   return (
     <>
       <Navbar />
       <Header />
+      <Ads />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
