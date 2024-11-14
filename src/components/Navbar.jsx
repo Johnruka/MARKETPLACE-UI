0332267f87ac3c642c482fc7d0bb8b10f8c0e3a6
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-11-14 14:42:51 +0100

    Implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,43 @@
 import React from "react";
-
-
-//rsr
-//rafce
+import logo from "../assets/Lexicon.svg";
+import styles from "../components/styles/Navbar.module.css";
 
 const Navbar = () => {
-  const navLinks = [
-    { id: 1, name: "LEXICON", href: "#" },
-    { id: 2, name: "Login", href: "#" },
-    { id: 3, name: "sign up", href: "#" },
-    { id: 4, name: "About", href: "#" },
+  const navbarLinks = [
+    { id: 1, name: "Login", href: "#" },
+    { id: 2, name: "Sign Up", href: "#" },
+    { id: 3, name: "About", href: "#" },
   ];
 
-  const renderNavLinks = () => {
+  const renderNavbarLinks = () => {
+    const listItems = navbarLinks.map((link) => {
+      const liElement = (
+        <li className="nav-item" id={link.id}>
+          <a className="nav-link" href={link.href}>
+            {link.name}
+          </a>
+        </li>
+      );
+      return liElement;
+    });
 
-   const listItems = navLinks.map( (link)=> {
-    const liElement = <li className="nav-item" >
-        <a className="nav-link" href={link.href}>
-        {link.name}
-        </a>
-    </li>;
-    return liElement;
-   });
-   
     return listItems;
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <span style={{ color: "red", fontWeight: "bold" }}>MyApp</span>
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-          {
-            renderNavLinks()
-          }           
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <a className="navbar-brand" href="#" >
+    <img src={logo} alt="Lexicon" id={styles.navbarLogo} />
+  </a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+        {renderNavbarLinks()}
+    </ul>
+  </div>
+</nav>
   );
 };
 
