5a108ebb0b6cd7321a96ab134b8c00075a50c0cb
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-11-16 11:27:44 +0100

    added independency

--- src/components/Advertisements.jsx
+++ src/components/Advertisements.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../components/styles/Ads.module.css'
-import AdCards from './AdCards'
+import AdCards from './adCards'
 const Ads = () => {
   return (
     <div className={styles.container} >
